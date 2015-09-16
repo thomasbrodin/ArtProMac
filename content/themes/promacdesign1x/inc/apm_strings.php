ce9143a1f38b07e2357f09411d27262e642c2d66
Thomas Brodin <thomasbrodin@gmail.com>
2015-09-16 15:50:51 -0400

    New strings and ACF

--- content/themes/promacdesign1x/inc/apm_strings.php
+++ content/themes/promacdesign1x/inc/apm_strings.php
@@ -1,14 +1,15 @@
 <?php
-pll_register_string('Artistes Title', 'Artistes', 'Artistes');
-pll_register_string('Artistes Liste', 'Liste', 'Artistes');
-pll_register_string('Artistes Thumbs', 'Vignettes', 'Artistes');
-pll_register_string('Artistes Details', 'Details', 'Artistes');
-pll_register_string('Artistes Slideshow', 'Diaporama', 'Artistes');
-pll_register_string('Artistes works', 'Selection d\'oeuvres', 'Artistes');
 pll_register_string('Artistes Bio', 'Biographie', 'Artistes');
 pll_register_string('Artistes Presse', 'Presse', 'Artistes');
-pll_register_string('Artistes Publications', 'Publications', 'Artistes');
-pll_register_string('Exposition', 'Expositions', 'Expositions');
+pll_register_string('Liste', 'Liste', 'Artistes & Expositions');
+pll_register_string('Artistes Title', 'Artistes', 'Artistes & Expositions');
+pll_register_string('Thumbnails', 'Vignettes', 'Artistes & Expositions');
+pll_register_string('Details', 'Details', 'Artistes & Expositions');
+pll_register_string('Slideshow', 'Diaporama', 'Artistes & Expositions');
+pll_register_string('Works', 'Selection d\'oeuvres', 'Artistes & Expositions');
+pll_register_string('Bibliographie', 'Bibliographie', 'Artistes');
+pll_register_string('Exposition Press', 'Dossier de presse', 'Expositions');
+pll_register_string('Exposition Catalogue', 'Catalogue', 'Expositions');
 pll_register_string('Exposition Current', 'Actuellement', 'Expositions');
 pll_register_string('Exposition Past', 'Passé', 'Expositions');
 pll_register_string('Exposition Upcoming', 'A venir', 'Expositions');
